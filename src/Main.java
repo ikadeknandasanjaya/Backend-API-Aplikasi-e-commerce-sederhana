655b2eafd2d1b0ed7c31b83832cdd93cd2449753
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-17 21:07:19 +0800

    mengkoneksikan database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String jdbcUrl = "jdbc:sqlite:/D:\\sqlite\\apiecommerce.db";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = "SELECT * FROM users";
+
+            Statement statement = connection.createStatement();
+
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                String first_name = result.getString("first_name");
+                String last_name = result.getString("last_name");
+                String email = result.getString("email");
+                String phone_number = result.getString("phone_number");
+                String type = result.getString("type");
+
+                System.out.println(first_name + " " + last_name + " " + email + " " + phone_number + " " + type);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error connecting to SQL Database");
+            e.printStackTrace();
+        }
     }
 }
