6b54a980872071754df9a82dca4f6c15010b682a
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-25 23:31:38 +0800

    membuat kelas Review di package model dengan berisi field dari isi tabel reviews pada apiecommerce.db

---
+++ src/main/java/model/Review.java
@@ -0,0 +1,46 @@
+package main.java.model;
+
+public class Review {
+    private int reviewId;
+    private int orderId;
+    private int star;
+    private String description;
+
+    public Review( String orderId, int star, String description) {
+        this.orderId = Integer.parseInt(orderId);
+        this.star = star;
+        this.description = description;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
