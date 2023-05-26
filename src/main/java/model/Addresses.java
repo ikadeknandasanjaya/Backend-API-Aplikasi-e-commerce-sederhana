d4d84438cf9cc40e05a8b1fc2e7285caf9559519
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-26 09:50:43 +0800

    membuat kelas Addresses untuk nantinya digunakan untuk mengisi lokasi dari users, dibuat dengan inisialisasi field lalu generate constructor dan setter getter dengan fitur generate dari intellij. Field dari Addresses disesuaikan column dari table addresses dari db eccomerce

---
+++ src/main/java/model/Addresses.java
@@ -0,0 +1,80 @@
+package main.java.model;
+
+public class Addresses {
+    private int users;
+    private String type;
+    private String line1;
+    private String line2;
+    private String city;
+    private String province;
+    private String postcode;
+
+    public Addresses(int users, String type, String line1, String line2, String city, String province, String postcode) {
+        this.users = users;
+        this.type = type;
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.province = province;
+        this.postcode = postcode;
+    }
+    public int getUsers() {
+        return users;
+    }
+
+    public void setUsers(int users) {
+        this.users = users;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+
+
+
+}
