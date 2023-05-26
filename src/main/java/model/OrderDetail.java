c557546d61e8f3fd2e4f94f07e6c7b0f7173bb4f
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-26 11:10:01 +0800

    membuat kelas OrderDetail pada package model dan berisi field sesuai dengan column dari table order_details

---
+++ src/main/java/model/OrderDetail.java
@@ -0,0 +1,29 @@
+package main.java.model;
+
+public class OrderDetail {
+        private int orderId;
+        private int productId;
+        private int quantity;
+        private int price;
+
+
+        public OrderDetail(String orderId, int productId, int quantity, int price) {
+            this.orderId = Integer.parseInt(orderId);
+            this.productId = productId;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public int getOrderId() {
+            return orderId;
+        }
+
+        public int getProductId() {
+            return productId;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+    }
+
