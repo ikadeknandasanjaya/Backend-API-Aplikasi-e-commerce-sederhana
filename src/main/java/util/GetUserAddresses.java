57a6c4aa6d66613146ad8784585382fc05077719
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-27 20:40:56 +0800

    membuat kelas GetUserAddresses untuk menangani kueri dan mengembalikan value untuk address dari users sesuai table pada basis data

---
+++ src/main/java/util/GetUserAddresses.java
@@ -0,0 +1,40 @@
+package main.java.util;
+
+import main.java.ECommerceAPI;
+import main.java.model.Address;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static main.java.ECommerceAPI.koneksi;
+
+public class GetUserAddresses {
+    public static List<Address> getUserAddresses(String userId) throws SQLException {
+        List<Address> addresses = new ArrayList<>();
+        ECommerceAPI api = new ECommerceAPI();
+        String query = "SELECT * FROM addresses WHERE users = ?";
+        try (Connection connection = api.koneksi();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, userId);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    String type = resultSet.getString("TYPE");
+                    String line1 = resultSet.getString("line1");
+                    String line2 = resultSet.getString("line2");
+                    String city = resultSet.getString("city");
+                    String province = resultSet.getString("province");
+                    String postcode = resultSet.getString("postcode");
+                    Address address = new Address(userId, type, line1, line2, city, province, postcode);
+                    addresses.add(address);
+                }
+            }
+        }
+        return addresses;
+    }
+}
+
