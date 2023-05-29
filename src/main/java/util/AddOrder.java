47eb95a27124fa7542d95b8380e9864ffad9d167
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-29 11:11:11 +0800

    menambahkan util AddOrder untuk menambahkan data order nantinya

---
+++ src/main/java/util/AddOrder.java
@@ -0,0 +1,26 @@
+package main.java.util;
+
+import main.java.ECommerceAPI;
+import main.java.model.Order;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddOrder {
+    public static boolean addOrder(Order order) throws SQLException {
+        ECommerceAPI api = new ECommerceAPI();
+        String query = "INSERT INTO orders (id, buyer, note, total, discount, is_paid) VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection connection = api.koneksi();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, order.getId());
+            statement.setString(2, order.getBuyer());
+            statement.setString(3, order.getNote());
+            statement.setDouble(4, order.getTotal());
+            statement.setDouble(5, order.getDiscount());
+            statement.setBoolean(6, order.isPaid());
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
+}
