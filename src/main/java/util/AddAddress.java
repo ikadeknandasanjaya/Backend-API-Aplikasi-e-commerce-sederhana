2d011a762fb3dbab7f76bca2691add628aa8c893
ikadeknandasanjaya <nandacomp1075@gmail.com>
2023-05-29 10:34:33 +0800

    menambahkan AddAddress sebagai util untuk menangani query untuk menambahkan alamat ke users

---
+++ src/main/java/util/AddAddress.java
@@ -0,0 +1,28 @@
+package main.java.util;
+
+import main.java.ECommerceAPI;
+import main.java.model.Address;
+import main.java.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddAddress {
+    public static boolean addAddress(Address address) throws SQLException {
+        ECommerceAPI api = new ECommerceAPI();
+        try (Connection connection = api.koneksi();) {
+            String query = "INSERT INTO users (type, line1, line2, city, province, postcode) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, address.getType());
+            statement.setString(2, address.getLine1());
+            statement.setString(3, address.getLine2());
+            statement.setString(4, address.getCity());
+            statement.setString(5, address.getProvince());
+            statement.setString(6, address.getPostcode());
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
+}
